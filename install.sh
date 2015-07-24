9f1b0c40aa757010925c32560b958a6717b67237
Julio Sueiras <juliosueiras@gmail.com>
2015-07-24 14:41:38 -0400

    Added tmux.conf and theme as well as updated install.sh

--- install.sh
+++ install.sh
@@ -17,12 +17,16 @@
 #      REVISION:  ---
 #===============================================================================
 
+
 read -p "This will overwrtie existings .vim .vimrc and .vimrc.addons in your folder , do you want to procced(y/N)" usr_answer
+
 if [[ $usr_answer == "y" ]]; then
     rm -rf ~/.vim ~/.vimrc ~/.vimrc.addons
     mv vim ~/.vim
     mv vimrc ~/.vimrc
     mv vimrc.addons ~/.vimrc.addons
+    mv theme/tmux.conf ~/.tmux.conf
+    mv theme/theme.tmuxline ~/.theme.tmuxline
     echo 'export PATH=$PATH:~/.vim/bin' >> ~/.login
     echo 'export PATH=$PATH:~/.vim/bin' >> ~/.bashrc
     exit 0;
